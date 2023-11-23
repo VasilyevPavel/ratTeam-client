af3dcb87096c9669ffc76e53c20420770b9862ec
Pavel Vasilyev <vasilps1989@mail.ru>
2023-11-23 12:06:18 +0000

    navbar started

--- ratteam/app/ui/Navbar.tsx
+++ ratteam/app/ui/Navbar.tsx
@@ -1,18 +1,20 @@
+import styles from "@/app/ui/navbar.module.css";
 import Image from "@/node_modules/next/image";
+import Link from "@/node_modules/next/link";
 import logo from "../../public/logo.png";
 export default function NavBar() {
   return (
-    <div>
-      <Image
-        src={logo}
-        width={500}
-        height={500}
-        alt="logo"
-        style={{
-          maxWidth: "100%",
-          height: "auto",
-        }}
-      />
+    <div className={styles.shape}>
+      <Image src={logo} width={100} alt="logo" className={styles.logo} />
+      <div className={styles.auth}>
+        <Link className={styles.loginLink} href="/info">
+          Войти
+        </Link>
+        или
+        <Link className={styles.regLink} href="/info">
+          Зарегистрироваться
+        </Link>
+      </div>
     </div>
   );
 }
