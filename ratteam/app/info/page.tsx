e1e98fd3ff3f3085bc1c48d4ee9af1b65829b2c9
Pavel Vasilyev <vasilps1989@mail.ru>
2023-11-24 10:54:17 +0000

    test done

--- ratteam/app/info/page.tsx
+++ ratteam/app/info/page.tsx
@@ -1,3 +1,17 @@
+"use client";
 export default function Info() {
-  return <div>Info</div>;
+  async function testHendler() {
+    try {
+      const res = await fetch(`https://5pg67c-3002.csb.app/api/auth/test`, {
+        method: "GET",
+        headers: { "Content-Type": "application/json" },
+        credentials: "include",
+      });
+      const test = await res.json();
+      console.log(test);
+    } catch (err) {
+      console.log(err);
+    }
+  }
+  return <button onClick={testHendler}>test</button>;
 }
